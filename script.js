d3483c759b91951236d9bd75059a7c5c1ab20c20
Mete <lllete@hotmail.com>
2020-04-13 11:46:36 +0300

    Alert ve trim eklendi.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
 
 
+
 eventListeners();
 
 function eventListeners() {
@@ -12,35 +13,49 @@ function eventListeners() {
   btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
+function showAlert(type, message) {
+  const alert = document.createElement("div")
+  alert.className = `alert alert-${type} mt-3`;
+  alert.textContent = message;
+  form.appendChild(alert);
 
-function addNewItem(e){
-  if(input.value === ""){
-    alert("add new item")
+  setTimeout(function(){
+    alert.remove();
+  },2500);
+}
+
+function addNewItem(e) {
+  if (input.value.trim() === "") {
+    showAlert("danger", "Add new item!");
   }
-  const li = document.createElement("li");
-  li.className = "list-group-item list-group-item-secondary";
-  li.appendChild(document.createTextNode(input.value));
-  
-  const a = document.createElement("a");
-  a.className = "delete-item float-right"
-  a.setAttribute("href","#");
-  a.innerHTML = '<i class="fas fa-times"></i>';
 
-  // add a to li
-  li.appendChild(a);
+  else {
+    const li = document.createElement("li");
+    li.className = "list-group-item list-group-item-secondary";
+    li.appendChild(document.createTextNode(input.value));
 
-  // add li to ul
-  taskList.appendChild(li);
+    const a = document.createElement("a");
+    a.className = "delete-item float-right"
+    a.setAttribute("href", "#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
 
-  // clear input
-  input.value = '';
+    // add a to li
+    li.appendChild(a);
 
+    // add li to ul
+    taskList.appendChild(li);
+
+    // clear input
+    input.value = '';
+    
+    showAlert("success", "Has been successfully added.");
+  }
   e.preventDefault();// sayfa yenilenmesin diye
 }
 
 
- function deleteItem(e) {
-  if (e.target.className === 'fas fa-times'){
+function deleteItem(e) {
+  if (e.target.className === 'fas fa-times') {
     e.target.parentElement.parentElement.remove();
   }
   e.preventDefault(); //sildiğimiz zaman scroll hareket etmesin diye
@@ -50,13 +65,13 @@ function addNewItem(e){
 function deleteAllItems(e) {
 
   if (confirm('are you sure ?')) {
-      taskList.innerHTML='';
+    taskList.innerHTML = '';
 
-      // taskList.childNodes.forEach(function (item) {
-      //     if (item.nodeType === 1) {
-      //         item.remove();
-      //     }
-      // });
+    // taskList.childNodes.forEach(function (item) {
+    //     if (item.nodeType === 1) {
+    //         item.remove();
+    //     }
+    // });
   }
   e.preventDefault();
 }
